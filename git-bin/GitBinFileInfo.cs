98ba9df9f7dd59e4fd25e0fd074ec8447254c7eb
Mark Dunham <mark@reentrygames.com>
2012-01-03 18:28:41 -0500

    Implemented equality members in GitBinFileInfo so that linq methods will work on collections of it

--- git-bin/GitBinFileInfo.cs
+++ git-bin/GitBinFileInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace GitBin
+﻿using System;
+
+namespace GitBin
 {
     public class GitBinFileInfo
     {
@@ -9,6 +11,29 @@
         {
             this.Name = name;
             this.Size = size;
+        }
+
+        public bool Equals(GitBinFileInfo other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.Name, Name) && other.Size == Size;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof(GitBinFileInfo)) return false;
+            return Equals((GitBinFileInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Name != null ? Name.GetHashCode() : 0) * 397) ^ Size.GetHashCode();
+            }
         }
     }
 }
